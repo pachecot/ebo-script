da5ccc5ba47ff76a037248acf4487e528c4d40fb
Tom Pacheco <tomp@netp.org>
2020-05-09 11:49:04 -0400

    change to single regexp on same string

--- src/ebo-scanner.ts
+++ src/ebo-scanner.ts
@@ -29,15 +29,16 @@ const SymbolMap = {
 } as { [name: string]: Symbols };
 
 
-const reWhiteSpace = /^\s+/;
-const reComment = /^(?:'.*)/;
-const reQuotedString = /^"(?:\|"|[^"|]*)*"/;
-const reNumber = /^(?:\d+(?:\.\d+)?(?:[eE][-+]?[0-9]+)?)/;
-const reTime = /^(?:\d{1,2}:\d{2}(?:\s*(i?:am|pm))?)/;
-const reKWords = new RegExp("^(?:" + Object.keys(EboKeyWords).filter(x => isNaN(Number(x))).join('|') + ")\\b", 'i');
-const reSymbol = /^(?:>=|<=|<>|[-,;!*&%^+<>=:~/\\()[\]])/;
-const reToken = /^[\w\d_]+\b/;
-const reErr = /^./;
+const reEndLine = /\r?\n/;
+const reWhiteSpace = /\s+/;
+const reComment = /'.*/;
+const reQuotedString = /"(?:\|"|[^"|]*)*"/;
+const reNumber = /\d+(?:\.\d+)?(?:[eE][-+]?[0-9]+)?/;
+const reTime = /\d{1,2}:\d{2}(?:\s*(?:am|pm))?/;
+const reSymbol = /(?:>=|<=|<>|[-,;!*&%^+<>=:~/\\()[\]])/;
+const reKWords = new RegExp("(?:" + Object.keys(EboKeyWords).filter(x => isNaN(Number(x))).join('|') + ")\\b");
+const reToken = /[\w_][\w\d_]+\b/;
+const reErr = /./;
 
 export type Token = LxToken | EboKeyWords | Symbols;
 
@@ -53,45 +54,50 @@ export interface LexToken {
     range: TextRange
 }
 
-const scannerMap: [RegExp, ((m: string) => Token)][] = [
-    [reWhiteSpace, () => LxToken.TK_WHITESPACE],
-    [reComment, () => LxToken.TK_COMMENT],
-    [reQuotedString, () => LxToken.TK_STRING],
-    [reNumber, () => LxToken.TK_NUMBER],
-    [reTime, () => LxToken.TK_TIME],
-    [reSymbol, (m: string) => SymbolMap[m] || LxToken.TK_OPERATOR],
-    [reKWords, (m: string) => EboKeyWords[m.toUpperCase() as any] as any as EboKeyWords || LxToken.TK_KEYWORD],
-    [reToken, () => LxToken.TK_IDENT],
-    [reErr, () => LxToken.TK_ERROR],
+const scannerRe = new RegExp(`(${[
+    // 0
+    reEndLine.source         // 1
+    , reWhiteSpace.source    // 2
+    , reComment.source       // 3
+    , reQuotedString.source  // 4
+    , reNumber.source        // 5
+    , reTime.source          // 6
+    , reSymbol.source        // 7
+    , reKWords.source        // 8
+    , reToken.source         // 9
+    , reErr.source           // 10
+].join(')|(')})`, 'yi');
+
+const scannerFns: ((m: string) => Token)[] = [
+/* 0*/    () => LxToken.TK_EOL,
+/* 1*/    () => LxToken.TK_WHITESPACE,
+/* 2*/    () => LxToken.TK_COMMENT,
+/* 3*/    () => LxToken.TK_STRING,
+/* 4*/    () => LxToken.TK_NUMBER,
+/* 5*/    () => LxToken.TK_TIME,
+/* 6*/    (m: string) => SymbolMap[m] || LxToken.TK_OPERATOR,
+/* 7*/    (m: string): Token => (EboKeyWords as any)[m.toUpperCase()] || LxToken.TK_KEYWORD,
+/* 8*/    () => LxToken.TK_IDENT,
+/* 9*/    () => LxToken.TK_ERROR,
 ];
 
-export function ebo_scan_line(line: string, line_id: number): LexToken[] {
+function ebo_scan_line(line: string, line_id: number): LexToken[] {
 
     const toks: LexToken[] = [];
-    let pos = 0;
-    let remain = line;
-
-    while (remain.length) {
-        for (let [re, fn] of scannerMap) {
-            let m = re.exec(remain);
-            if (m) {
-                let sz = m[0].length;
-                toks.push({
-                    range: {
-                        line: line_id,
-                        begin: pos,
-                        end: pos + sz,
-                    },
-                    type: fn(m[0]),
-                    value: m[0]
-                });
-                pos += sz;
-                remain = remain.substring(sz);
-                break;
-            }
-        }
+    let m: RegExpExecArray | null;
+    while ((m = scannerRe.exec(line))) {
+        let i = 1;
+        while (!m[i] && i < m.length) { ++i; }
+        toks.push({
+            range: {
+                line: line_id,
+                begin: m.index,
+                end: m.index + m[i].length
+            },
+            type: scannerFns[i - 1](m[i]),
+            value: m[i]
+        });
     }
-
     toks.push({
         range: {
             line: line_id,
@@ -104,7 +110,7 @@ export function ebo_scan_line(line: string, line_id: number): LexToken[] {
     return toks;
 }
 
-export function ebo_scan_lines(lines: string[]) {
+function ebo_scan_lines(lines: string[]) {
     return lines.map(ebo_scan_line);
 }
 
@@ -113,5 +119,5 @@ export function ebo_scan_lines(lines: string[]) {
  * @param fileText 
  */
 export function ebo_scan_text(fileText: string) {
-    return ebo_scan_lines(fileText.split('\r\n'));
+    return ebo_scan_lines(fileText.split(reEndLine));
 }
