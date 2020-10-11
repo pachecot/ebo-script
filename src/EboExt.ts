d41315b639f306825154480ff130ed1413769524
Tom Pacheco <tomp@netp.org>
2020-10-11 10:01:55 -0400

    rafactored ext

--- src/EboExt.ts
+++ src/EboExt.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import * as ebo from './ebo-check';
 import { EBO_SCRIPT } from './extension-types';
+import { SymbolTable } from './SymbolTable';
 import { SymbolTableCollection } from './SymbolTableMap';
 
 export class EboExt {
@@ -20,49 +21,62 @@ export class EboExt {
     }
 
     update(document: vscode.TextDocument): void {
-
         if (document && document.languageId === EBO_SCRIPT) {
-
-            const ast = ebo.ebo_parse_file(document.getText());
-            const issues = ast.errors.map(issue => (
-                {
-                    code: issue.id,
-                    message: issue.message,
-                    range: new vscode.Range(
-                        issue.range.line, issue.range.begin,
-                        issue.range.line, issue.range.end
-                    ),
-                    severity: issue.severity as unknown as vscode.DiagnosticSeverity,
-                    source: ''
-                }
-            ));
-
-            this.symbols.set(document.uri, ast);
-            this.collection.set(document.uri, issues);
+            this.update_file(document.uri, document.getText());
         }
     }
 
+    update_file(uri: vscode.Uri, fileText: string): void {
+        const st = ebo_parse_file(fileText);
+        this.update_ast(uri, st);
+    }
+
+
+    update_ast(uri: vscode.Uri, ast: SymbolTable): void {
+
+        const issues = ast.errors.map(issue => (
+            {
+                code: issue.id,
+                message: issue.message,
+                range: new vscode.Range(
+                    issue.range.line, issue.range.begin,
+                    issue.range.line, issue.range.end
+                ),
+                severity: issue.severity as unknown as vscode.DiagnosticSeverity,
+                source: ''
+            }
+        ));
+
+        this.symbols.set(uri, ast);
+        this.collection.set(uri, issues);
+    }
+    }
+
+
+
+    workspace_onDidDeleteFiles(fileDeleteEvent: vscode.FileDeleteEvent) {
+        fileDeleteEvent.files.forEach(file => this.delete(file));
+    }
+
+    workspace_onDidSaveTextDocument(document: vscode.TextDocument) {
+        this.update(document);
+    }
+
+    workspace_onDidChangeTextDocument(e: vscode.TextDocumentChangeEvent) {
+        this.update(e.document);
+    }
+
+    window_onDidChangeActiveTextEditor(editor: vscode.TextEditor | undefined) {
+        if (editor) {
+            this.update(editor.document);
+        }
+    }
 
     subscribe(context: vscode.ExtensionContext) {
-
-        const subscriptions = [
-            
-            vscode.workspace.onDidDeleteFiles(fileDeleteEvent => {
-                fileDeleteEvent.files.forEach(file => this.delete(file));
-            }),
-            
-            vscode.workspace.onDidSaveTextDocument(document => { this.update(document); }),
-
-            vscode.workspace.onDidChangeTextDocument(e => { this.update(e.document); }),
-
-            vscode.window.onDidChangeActiveTextEditor(editor => {
-                if (editor) {
-                    this.update(editor.document);
-                }
-            })
-        ];
-
-        context.subscriptions.splice(context.subscriptions.length, 0, ...subscriptions);
+        vscode.window.onDidChangeActiveTextEditor(this.window_onDidChangeActiveTextEditor, this, context.subscriptions);
+        vscode.workspace.onDidChangeTextDocument(this.workspace_onDidChangeTextDocument, this, context.subscriptions);
+        vscode.workspace.onDidDeleteFiles(this.workspace_onDidDeleteFiles, this, context.subscriptions);
+        vscode.workspace.onDidSaveTextDocument(this.workspace_onDidSaveTextDocument, this, context.subscriptions);
     }
 
 }
